a64e233883b9cf2d14f406ef6dc54ad39ce8b550
William Redding <williamdredding@gmail.com>
2022-11-05 13:39:03 +0000

    Removed leaderboard command functionality

--- src/cogs/rankings.py
+++ src/cogs/rankings.py
@@ -82,38 +82,6 @@ class Rankings(commands.Cog):
             await ctx.send("Invalid page number!")
             return
 
-        async def id_name(id):
-            nonlocal id_name_dict
-            name = str(await self.bot.fetch_user(id))
-            id_name_dict[id] = name
-
-        page -= 1
-
-        cursor = database.user_data.find({})
-
-        inventory_value_dict = {}
-
-        for document in cursor:
-            inventory_value = 0
-            for item in list(document["inventory"]):
-                inventory_value += database.skin_data[item["name"]]["price"]
-            inventory_value_dict[inventory_value] = document["_id"]
-
-        id_name_dict = {}
-        ordered_prices = sorted(inventory_value_dict, reverse=True)[page*10:(page+1)*10-1]
-        leaderboard_data = {ordered_price: inventory_value_dict[ordered_price] for ordered_price in ordered_prices}
-        tasks = [id_name(id) for id in leaderboard_data.values()]
-        await asyncio.gather(*tasks)
-
-        leaderboard_str = ""
-        for count, (price, id) in enumerate(leaderboard_data.items()):
-            price = str((Decimal(price) / 100).quantize(Decimal('0.01')))
-            leaderboard_str += f"**{page * 10 + count+1})** {id_name_dict[id]}: ${price}\n"
-
-        e = discord.Embed(title=f"Leaderboard - Page {page+1}/{amount_of_pages}", description=leaderboard_str)
-
-        await ctx.send(embed=e)
-
     @leaderboard.error
     async def leaderboard_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
