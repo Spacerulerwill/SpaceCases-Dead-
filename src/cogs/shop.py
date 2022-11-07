b99ec5b405a02218b3626e87a03acb623a45e70b
William Redding <williamdredding@gmail.com>
2022-11-07 13:15:05 +0000

    Shop cog setup finish

--- src/cogs/shop.py
+++ src/cogs/shop.py
@@ -14,14 +14,24 @@ class Shop(commands.Cog):
     @commands.group(pass_context=True, description="Open the shop menu to see what items are on sale", usage=f"""`{PREFIX}shop`""")
     async def shop(self, ctx):
         if ctx.invoked_subcommand is None:
-            await ctx.send('Invalid sub command!')
+            pass
+        else:
+            pass
 
     @shop.command(description="Buy an item for sale in the shop", usage=f"""`{PREFIX}shop buy <item number>`
     **Arguments**
     `<item number>` - the number of the item you want to buy
     """)
-    async def buy(self, ctx):
-        await ctx.send("")
+    async def buy(self, ctx, item_number:int):
+        pass
+
+    @buy.error
+    async def buy_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("Item number must be an integer!")
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Oops! You forgot to put the item number!")
+
 # this setup function needs to be in every cog in order for the bot to be able to load it
 async def setup(bot):
     await bot.add_cog(Shop(bot))
