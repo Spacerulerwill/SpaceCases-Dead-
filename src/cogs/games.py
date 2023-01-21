cf66c23c6537e297a1756dee273420c3f626d740
William Redding <williamdredding@gmail.com>
2023-01-21 21:18:22 +0000

    Fixed skin? help menu showing incorrect prices

--- src/cogs/games.py
+++ src/cogs/games.py
@@ -9,9 +9,10 @@ This cog contains the commands:
 from discord.ext import commands
 from discord.ext.commands import Context
 from src.util.constants import PREFIX
+from src.util.string_util import currency_str_format
 
 # commands
-from src.commands.games.skin_game import skin_game
+from src.commands.games.skin_game import skin_game, SKIN_GAME_PRICE, SKIN_GAME_REWARD
 
 # initialise class
 class Games(commands.Cog):
@@ -21,9 +22,9 @@ class Games(commands.Cog):
     @commands.command(name="skin?", description="Play the skin guessing game!", usage=
     {
         "Syntax": f"{PREFIX}skin?",
-        "How To Play": """Use the command and reply to the message with the name of the skin **within 10 seconds!** Do **not** include the condition or the name of the weapon, just the skin name. 
+        "How To Play": f"""Use the command and reply to the message with the name of the skin **within 10 seconds!** Do **not** include the condition or the name of the weapon, just the skin name. 
         
-        It costs **$10** to play, winning rewards you with **$40!**"""
+        It costs **{currency_str_format(SKIN_GAME_PRICE)}** to play, winning rewards you with **{currency_str_format(SKIN_GAME_REWARD)}!**"""
     })
     async def skin_game(self, ctx:Context):
         await skin_game(ctx)
