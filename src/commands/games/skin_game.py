57d1b84cbaa51b8043c962f1a41254fa81af03bf
William Redding <williamdredding@gmail.com>
2023-01-21 21:21:42 +0000

    Fixed unboundLocal error with skin_name in skin? command

--- src/commands/games/skin_game.py
+++ src/commands/games/skin_game.py
@@ -44,7 +44,8 @@ async def skin_game(ctx:Context):
 
     random_item = random.choice(list(database.skin_data["skins"].keys()))
     random_item_data = database.skin_data["skins"][random_item]
-
+    skin_name = remove_skin_name_formatting(random_item_data["formatted_name"].split(" | ")[1]).strip()
+    
     image_url = random_item_data["image_url"]
 
     e.set_image(url=image_url)
@@ -57,7 +58,6 @@ async def skin_game(ctx:Context):
     try:
         response:discord.Message = await ctx.bot.wait_for('message', check=check, timeout=10)
         
-        skin_name = remove_skin_name_formatting(random_item_data["formatted_name"].split(" | ")[1]).strip()
 
         guess = response.content.strip().lower()
 
