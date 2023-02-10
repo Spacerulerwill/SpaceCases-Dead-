0f3db170ff7de5010047144360e20ba6f2b9adfc
William Redding <williamdredding@gmail.com>
2023-02-10 19:34:53 +0000

    Fix

--- src/commands/unbox/containers.py
+++ src/commands/unbox/containers.py
@@ -9,7 +9,7 @@ containerlist_pages = [
     (
         "Cases",
         [
-            "revolution case"
+            "revolution case",
             "operation riptide case",
             "snakebite case",
             "operation broken fang case",
