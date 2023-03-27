d0f837f85d0c29e5001cb0a15490b45375a16b07
William Redding <williamdredding@gmail.com>
2023-03-27 19:47:43 +0100

    Lang support help command

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -10,6 +10,9 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Context
 from src.util.constants import PREFIX
+from src.util.lang import get_locale
+from src.util import database 
+from src.util.string_util import get_closest_match
 from src.util.embed_func import msg_embed
 
 # initialise class
@@ -21,11 +24,19 @@ class Help(commands.Cog):
     # If a command is specified with display the description and usage details of that command
     @commands.command()
     async def help(self, ctx:Context, *args):
+        user_data = database.user_data.find_one({"_id": ctx.author.id})
+
+        if user_data is None:
+            lang = "en"
+        else:
+            lang = user_data["language"]
+
+
         command_name = " ".join(args[:]).strip().lower()
 
         if command_name == "":
             e = discord.Embed(
-                description=f'Use `{PREFIX}help <command>` to gain more information about that command', 
+                description=get_locale("help.embed.description", PREFIX), 
                 color=discord.Color.dark_theme()
             )
             e.set_thumbnail(url=self.bot.user.display_avatar.url)
@@ -44,52 +55,57 @@ class Help(commands.Cog):
                     e.add_field(name=cog, value=field_value)
             await ctx.send(embed=e)
         else:       
-            command_name = command_name.split(" ")  
-            
-            #command
-            if len(command_name) == 1:
-                command_name = command_name[0]
-                command:commands.Command = self.bot.get_command(command_name)
-                if command == None or command.name == "help":
-                    await msg_embed(ctx, "Invalid command!")
-                    return
+
+            try:
+                command_name = command_name.split(" ")  
                 
-                e = discord.Embed(title=f"{PREFIX}{command_name}", description=command.description, color=discord.Color.dark_theme())
-                e.set_thumbnail(url=self.bot.user.display_avatar.url)
+                #command
+                if len(command_name) == 1:
+                    command_name = command_name[0]
+                    command:commands.Command = self.bot.get_command(command_name)
 
-                for name, value in command.usage.items():
-                    e.add_field(name=name, value=value, inline=False)
+                    e = discord.Embed(title=f"{PREFIX}{command_name}", description=command.description, color=discord.Color.dark_theme())
+                    e.set_thumbnail(url=self.bot.user.display_avatar.url)
 
-                if len(command.aliases) > 0:
-                    alias_str = ""
-                    for alias in command.aliases:
-                        alias_str += f"\u200b\t•{alias}\n"
-                    e.add_field(name="Aliases", value=alias_str, inline=False)
+                    for name, value in command.usage.items():
+                        e.add_field(name=name, value=value, inline=False)
 
-                await ctx.send(embed=e)
+                    if len(command.aliases) > 0:
+                        alias_str = ""
+                        for alias in command.aliases:
+                            alias_str += f"\u200b\t•{alias}\n"
+                        e.add_field(name=get_locale(lang, "help.aliases"), value=alias_str, inline=False)
 
-            elif len(command_name) == 2: # command with group
-                group_name = command_name[0]
-                subcommand_name = command_name[1]
-                group:commands.Group = self.bot.get_command(group_name)
-                subcommand = group.get_command(subcommand_name)
+                    await ctx.send(embed=e)
 
-                e = discord.Embed(title=f"{PREFIX}{group_name} {subcommand_name}", description=subcommand.description, color=discord.Color.dark_theme())
-                e.set_thumbnail(url=self.bot.user.display_avatar.url)
+                elif len(command_name) == 2: # command with group
 
-                for name, value in subcommand.usage.items():
-                    e.add_field(name=name, value=value, inline=False)
+                    group_name = command_name[0]
+                    subcommand_name = command_name[1]
+                    group:commands.Group = self.bot.get_command(group_name)
+                    subcommand = group.get_command(subcommand_name)
 
-                if len(subcommand.aliases) > 0:
-                    alias_str = ""
-                    for alias in subcommand.aliases:
-                        alias_str += f"\u200b\t•{alias}\n"
-                    e.add_field(name="Aliases", value=alias_str, inline=False)
+                    e = discord.Embed(title=f"{PREFIX}{group_name} {subcommand_name}", description=subcommand.description, color=discord.Color.dark_theme())
+                    e.set_thumbnail(url=self.bot.user.display_avatar.url)
 
-                await ctx.send(embed=e)
-            else:
-                await msg_embed(ctx, "Invalid command!")
+                    for name, value in subcommand.usage.items():
+                        e.add_field(name=name, value=value, inline=False)
 
+                    if len(subcommand.aliases) > 0:
+                        alias_str = ""
+                        for alias in subcommand.aliases:
+                            alias_str += f"\u200b\t•{alias}\n"
+                        e.add_field(name=get_locale(lang, "help.aliases"), value=alias_str, inline=False)
+
+                    await ctx.send(embed=e)
+            except AttributeError:
+                closest_match = get_closest_match(command_name, self.bot.commands)
+                if closest_match is None:
+                    await msg_embed(ctx, get_locale(lang, "command_not_found"))
+                else:
+                    await msg_embed(ctx, get_locale(lang, "command_not_found_suggest", closest_match))
+
+    
 # this setup function needs to be in every cog in order for the bot to be able to load it
 async def setup(bot):
     await bot.add_cog(Help(bot))
